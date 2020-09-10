7f85ab29aa43c8a4b5a6e1c6eb0cb8ffa28d3b56
car2854 <39568391+car2854@users.noreply.github.com>
2020-09-10 14:34:13 -0400

    Corregir la vista al crear un Bien

--- resources/views/bienes/bien/create.blade.php
+++ resources/views/bienes/bien/create.blade.php
@@ -20,7 +20,7 @@
                     <div class="card">
                         <div class="card-body">
                             <div class="basic-form">
-                                <div style="flex-wrap: initial" class="row">
+                                <div class="row">
                                     <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
                                         @if (count($errors)>0)
                                         <div class="alert alert-danger">
