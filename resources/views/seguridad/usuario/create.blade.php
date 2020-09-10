6c4ecdc073dbabc179dc6f923a5adfb17e426186
car2854 <39568391+car2854@users.noreply.github.com>
2020-09-10 16:32:07 -0400

    Arreglar la vista para agregar un Usuario

--- resources/views/seguridad/usuario/create.blade.php
+++ resources/views/seguridad/usuario/create.blade.php
@@ -35,69 +35,49 @@
                                     {!!Form::open(array('url'=>'seguridad/usuario','method'=>'POST','autocomplete'=>'off'))!!}
                                     {{Form::token()}}
 
-                                        <div class="form-group row">
+                                        <div class="form-group">
                                             <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Nombre') }}</label>
 
-                                            <div class="col-md-6">
-                                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                                            <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
 
-                                                @error('name')
-                                                    <span class="invalid-feedback" role="alert">
-                                                        <strong>{{ $message }}</strong>
-                                                    </span>
-                                                @enderror
-
-
-
-                                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                                @error('email')
-                                                    <span class="invalid-feedback" role="alert">
-                                                        <strong>{{ $message }}</strong>
-                                                    </span>
-                                                @enderror
-                                            </div>
                                         </div>
 
+                                        <div class="form-group">
+                                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail ') }}</label>
 
 
-                                        <div class="form-group row">
+                                            <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+
+
+                                        </div>
+
+                                        <div class="form-group">
                                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
-                                            <div class="col-md-6">
-                                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+                                            <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
 
-                                                @error('password')
-                                                    <span class="invalid-feedback" role="alert">
-                                                        <strong>{{ $message }}</strong>
-                                                    </span>
-                                                @enderror
-                                            </div>
                                         </div>
 
-                                        <div class="form-group row">
+                                        <div class="form-group">
                                             <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirmar Password') }}</label>
 
-                                            <div class="col-md-6">
                                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                                            </div>
+
                                         </div>
 
                                         <!--
                                         Desde aqui la modificacion
                                         -->
 
-                                        <div class="form-group row">
+                                        <div class="form-group">
                                             <label class="col-md-4 col-form-label text-md-right">Rol</label>
-                                                <div class="col-md-6">
 
-                                                    <select class="form-control" name="rol">
-                                                        @foreach($roles as $key => $value)
-                                                            <option value="{{$value}}">{{$value}}</option>
-                                                        @endforeach
-                                                    </select>
+                                            <select class="form-control" name="rol">
+                                                @foreach($roles as $key => $value)
+                                                    <option value="{{$value}}">{{$value}}</option>
+                                                @endforeach
+                                            </select>
 
-                                                </div>
                                         </div>
 
 
@@ -118,5 +98,6 @@
         </div>
     </div>
 </div>
+
 
 @endsection
