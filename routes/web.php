3ff27f465d63ccae70f6999c8e14d2a37e608182
car2854 <39568391+car2854@users.noreply.github.com>
2020-09-09 20:18:58 -0400

    agregar ruta

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -10,7 +12,6 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('/', function () {
     return view('auth/login');
 });
