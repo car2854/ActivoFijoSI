4f34dbf5cd6de6d2095cc3c1400b8d7272a5d8ba
car2854 <39568391+car2854@users.noreply.github.com>
2020-09-10 14:51:55 -0400

    Corregir la vista al crear un Bien

--- resources/views/bienes/bien/create.blade.php
+++ resources/views/bienes/bien/create.blade.php
@@ -36,7 +36,7 @@
                                         {{Form::token()}}
 
                                         <div class="row">
-                                            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                                            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
 
                                                 <div class="form-group">
                                                     <label for="CodBien">Codigo</label>
@@ -50,7 +50,7 @@
 
                                             </div>
 
-                                            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                                            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
 
                                                 <div class="form-group">
                                                     <label for="FechaAdquisicion">Fecha de Adquisicion</label>
