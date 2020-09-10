96d8017c3a8dffd4c5db90c27b482503b1df2907
car2854 <39568391+car2854@users.noreply.github.com>
2020-09-10 15:22:53 -0400

    Arreglar la vista de dar Baja

--- resources/views/RevisionTecnica/Baja/Create.blade.php
+++ resources/views/RevisionTecnica/Baja/Create.blade.php
@@ -1,40 +1,69 @@
 @extends ('layouts.principal')
 @section ('contenido')
-  <div class="row">
-    <div  class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-      <h3>Dar de Baja</h3>
-      @if (count($errors)>0)
-      <div class="alert alert-danger">
-        <ul>
-        @foreach ($errors->all() as $error)
-          <li>{{$error}}</li>
-        @endforeach
-        </ul>
-      </div>
-      @endif
+
+<div class="content-wrap">
+    <div class="main">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-8 p-r-0 title-margin-right">
+                    <div class="page-header">
+                        <div class="page-title">
+                            <h1>Dar de Baja</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- /# row -->
+            <section id="main-content">
 
 
-      {!!Form::open(array('url'=>'RevisionTecnica/Baja','method'=>'POST','autocomplete'=>'off','route'=>array('route.name',$NroR)))!!}
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="basic-form">
+                                <div class="row">
+                                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                                        @if (count($errors)>0)
+                                        <div class="alert alert-danger">
+                                            <ul>
+                                            @foreach ($errors->all() as $error)
+                                                <li>{{$error}}</li>
+                                            @endforeach
+                                            </ul>
+                                        </div>
+                                        @endif
 
-      {{Form::token()}}
 
-      <div class="form-group">
-        <label for="Descripcion">Descripcion</label>
-        <input type="text" name="Descripcion" class="form-control" value="" placeholder="Descripcion...">
+                                        {!!Form::open(array('url'=>'RevisionTecnica/Baja','method'=>'POST','autocomplete'=>'off','route'=>array('route.name',$NroR)))!!}
 
-        <input type="text" name="NroRevision" class="form-control" value="{{$NroR}}" style="visibility:hidden">
+                                        {{Form::token()}}
 
-      </div>
+                                        <div class="form-group">
+                                            <label for="Descripcion">Descripcion</label>
+                                            <input type="text" name="Descripcion" class="form-control" value="" placeholder="Descripcion...">
+
+                                            <input type="text" name="NroRevision" class="form-control" value="{{$NroR}}" style="visibility:hidden">
+
+                                        </div>
+
+                                        <div class="form-group">
+                                            <button class="btn btn-primary" type="submit">Guardar</button>
+                                            <button class="btn btn-danger" type="reset">Cancelar</button>
+                                        </div>
+
+                                        {!!Form::close()!!}
 
 
 
-      <div class="form-group">
-        <button class="btn btn-primary" type="submit">Guardar</button>
-        <button class="btn btn-danger" type="reset">Cancelar</button>
-      </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
 
-      {!!Form::close()!!}
 
+            </section>
+        </div>
     </div>
-  </div>
+</div>
+
 @endsection
