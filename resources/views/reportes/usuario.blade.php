179103aa0dc0ecacd7324157dbc701e6dd197e3b
car2854 <39568391+car2854@users.noreply.github.com>
2020-09-11 15:57:20 -0400

    Arreglar diseño PDF

--- resources/views/reportes/usuario.blade.php
+++ resources/views/reportes/usuario.blade.php
@@ -8,7 +8,7 @@
 
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="{{ asset('css/style.css') }}">
 </head>
 <body>
 	<div id="page-wrap">
