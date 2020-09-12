ee5a3445dedc8f25ab1cc4e5dd7782106979ad76
car2854 <39568391+car2854@users.noreply.github.com>
2020-09-11 21:00:57 -0400

    Arreglar estilos PDF

--- resources/views/reportes/adquisicion.blade.php
+++ resources/views/reportes/adquisicion.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>REPORTE DE CATEGORIAS</title>
-
+    <link rel="stylesheet" href="assets/css/estilospdf.css">
   </head>
 <body>
   <header>
