20cbb9d5e037a8da8ac56fa32ca6547eee4f3828
car2854 <39568391+car2854@users.noreply.github.com>
2020-09-14 13:04:52 -0400

    Arreglando vista ubicacion

--- resources/views/ubicacion/ubicacion/create.blade.php
+++ resources/views/ubicacion/ubicacion/create.blade.php
@@ -1,45 +1,79 @@
 @extends('layouts.principal')
 @section('contenido')
-    <div class=" col-lg-12 col-md-12 col-sm-12 col-xs-12">
-            <h3>Registrar Ubicacion</h3>
-            @if (count($errors)>0)
-            <div class="alert alert-danger">
-                <ul>
-                    @foreach ($errors->all() as $error )
-                        <li>{{$error}}</li>
-                    @endforeach
-                </ul>
-            </div>                
-            @endif
-    
-    {!!Form::open(array('url'=>'ubicacion/ubicacion','method'=>'POST','autocomplete'=>'off'))!!}
-    {{Form::token()}}
-    <div class="row col-lg-12 col-sm-12 col-md-12 col-xs-12">
-            
-        <div class="col-lg-6 col-sm-6 col-md-6 col-xs-12">
-            <div class="form-group">
-                    <label for="edificio">Nombre Edificio</label>
-                    <input type="text" name="edificio" required value="{{old('nombre')}}" class="form-control" placeholder="Nombre...">
-            </div>
-        </div>
-        <div class="col-lg-6 col-sm-6 col-md-6 col-xs-12">
-            <div class="form-group">
-                <label for="ciudad">Ciudad</label>
-                <input type="text" name="ciudad" required value="{{old('nombre')}}" class="form-control" placeholder="ciudad...">
-            </div>
-        </div>
-        <div class="col-lg-6 col-sm-6 col-md-6 col-xs-12">
-            <div class="form-group">
-                <label for="pais">Pais</label>
-                <input type="text" value="{{old('nombre')}}" name="pais" class="form-control" placeholder="pais">
-            </div>
-        </div>
-    </div>
-    <div class="form-group col-md-6">
-            <button type="submit" class="btn btn-primary">Guardar</button>
-            <button type="reset" class="btn btn-danger">Cancelar</button>
-    </div>
 
-    {!!Form::close()!!}
+
+
+<div class="content-wrap">
+    <div class="main">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-8 p-r-0 title-margin-right">
+                    <div class="page-header">
+                        <div class="page-title">
+                            <h1>Registrar Ubicacion</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- /# row -->
+            <section id="main-content">
+
+                <div class="card">
+                    <div class="card-body">
+                        <div class="basic-form">
+                            <div class="row">
+                                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                                    @if (count($errors)>0)
+                                    <div class="alert alert-danger">
+                                        <ul>
+                                        @foreach ($errors->all() as $error)
+                                            <li>{{$error}}</li>
+                                        @endforeach
+                                        </ul>
+                                    </div>
+                                    @endif
+
+                                    {!!Form::open(array('url'=>'ubicacion/ubicacion','method'=>'POST','autocomplete'=>'off'))!!}
+                                    {{Form::token()}}
+                                    <div class="row col-lg-12 col-sm-12 col-md-12 col-xs-12">
+
+                                        <div class="col-lg-6 col-sm-6 col-md-6 col-xs-12">
+                                            <div class="form-group">
+                                                    <label for="edificio">Nombre Edificio</label>
+                                                    <input type="text" name="edificio" required value="{{old('nombre')}}" class="form-control" placeholder="Nombre...">
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-6 col-sm-6 col-md-6 col-xs-12">
+                                            <div class="form-group">
+                                                <label for="ciudad">Ciudad</label>
+                                                <input type="text" name="ciudad" required value="{{old('nombre')}}" class="form-control" placeholder="ciudad...">
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-6 col-sm-6 col-md-6 col-xs-12">
+                                            <div class="form-group">
+                                                <label for="pais">Pais</label>
+                                                <input type="text" value="{{old('nombre')}}" name="pais" class="form-control" placeholder="pais">
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="form-group col-md-6">
+                                            <button type="submit" class="btn btn-primary">Guardar</button>
+                                            <button type="reset" class="btn btn-danger">Cancelar</button>
+                                    </div>
+
+                                    {!!Form::close()!!}
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </section>
+        </div>
+    </div>
 </div>
+
+
+
 @endsection
