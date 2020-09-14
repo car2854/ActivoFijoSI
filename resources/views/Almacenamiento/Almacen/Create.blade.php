16762bf736b31daf0d18299e2b7f248537e8e04a
car2854 <39568391+car2854@users.noreply.github.com>
2020-09-14 13:13:57 -0400

    Arreglando vista almacen

--- resources/views/Almacenamiento/Almacen/Create.blade.php
+++ resources/views/Almacenamiento/Almacen/Create.blade.php
@@ -1,38 +1,67 @@
 @extends('layouts.principal')
 @section ('contenido')
-  <div class="row">
-    <div  class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-      <h3>Nueva Categoria</h3>
-      @if (count($errors)>0)
-      <div class="alert alert-danger">
-        <ul>
-        @foreach ($errors->all() as $error)
-          <li>{{$error}}</li>
-        @endforeach
-        </ul>
-      </div>
-      @endif
 
-      {!!Form::open(array('url'=>'Almacenamiento/Almacen','method'=>'POST','autocomplete'=>'off'))!!}
-      {{Form::token()}}
 
-      <div class="form-group">
-        <label for="NroAlmacen">Numero Almacen</label>
-        <input type="text" name="NroAlmacen" class="form-control" placeholder="Numero Almacen...">
-      </div>
+<div class="content-wrap">
+    <div class="main">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-8 p-r-0 title-margin-right">
+                    <div class="page-header">
+                        <div class="page-title">
+                            <h1>Nueva Almacen</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- /# row -->
+            <section id="main-content">
 
-      <div class="form-group">
-        <label for="Direccion">Direccion</label>
-        <input type="text" name="Direccion" class="form-control" placeholder="Direccion...">
-      </div>
+                <div class="card">
+                    <div class="card-body">
+                        <div class="basic-form">
+                            <div class="row">
+                                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                                    @if (count($errors)>0)
+                                    <div class="alert alert-danger">
+                                        <ul>
+                                        @foreach ($errors->all() as $error)
+                                            <li>{{$error}}</li>
+                                        @endforeach
+                                        </ul>
+                                    </div>
+                                    @endif
 
-      <div class="form-group">
-        <button class="btn btn-primary" type="submit">Guardar</button>
-        <button class="btn btn-danger" type="reset">Cancelar</button>
-      </div>
+                                    {!!Form::open(array('url'=>'Almacenamiento/Almacen','method'=>'POST','autocomplete'=>'off'))!!}
+                                    {{Form::token()}}
 
-      {!!Form::close()!!}
+                                    <div class="form-group">
+                                        <label for="NroAlmacen">Numero Almacen</label>
+                                        <input type="text" name="NroAlmacen" class="form-control" placeholder="Numero Almacen...">
+                                    </div>
 
+                                    <div class="form-group">
+                                        <label for="Direccion">Direccion</label>
+                                        <input type="text" name="Direccion" class="form-control" placeholder="Direccion...">
+                                    </div>
+
+                                    <div class="form-group">
+                                        <button class="btn btn-primary" type="submit">Guardar</button>
+                                        <button class="btn btn-danger" type="reset">Cancelar</button>
+                                    </div>
+
+                                    {!!Form::close()!!}
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </section>
+        </div>
     </div>
-  </div>
+</div>
+
+
 @endsection
