8b05f8ac48eba13b4755ef7584d17b8d3c0bf08d
car2854 <39568391+car2854@users.noreply.github.com>
2020-09-30 16:31:48 -0400

    Agregando vista Revaluo

--- resources/views/RevisionTecnica/Revaluo/index.blade.php
+++ resources/views/RevisionTecnica/Revaluo/index.blade.php
@@ -1,44 +1,70 @@
 @extends ('layouts.principal')
 @section ('contenido')
 
-<div class="content">
-    <div class="card-body">
-        <h4 class="box-title">Listado Revaluo</h4>
-        @include('RevisionTecnica.Mantenimiento.Search')
+
+<div class="row">
+    <div class="col-lg-8 p-r-0 title-margin-right">
+        <div class="page-header">
+            <div class="page-title">
+                <h1>Listado Revaluo</h1>
+            </div>
+        </div>
     </div>
-    <div class="card-body--">
-        <div class="table-stats order-table ov-h">
-            <table class="table table-striped table-bordered table-condensed table-hover">
-                <thead>
-                    <tr>
-                      <th class="col-lg-1 col-md-1 col-sm-1 col-xs-1">Cod. Bien</th>
-                      <th class="col-lg-1 col-md-1 col-sm-1 col-xs-1">Nombre Bien</th>
-                      <th class="col-lg-1 col-md-1 col-sm-1 col-xs-1">Custodio</th>
-                      <th class="col-lg-1 col-md-1 col-sm-1 col-xs-1">Operador</th>
-                      <th class="col-lg-1 col-md-1 col-sm-1 col-xs-1">Estado</th>
-                      <th class="col-lg-1 col-md-1 col-sm-1 col-xs-1">FechaHora</th>
-                      <th class="col-lg-1 col-md-1 col-sm-1 col-xs-1">Monto</th>
-                      <th class="col-lg-1 col-md-1 col-sm-1 col-xs-1">Descripcion</th>
-                    </tr>
-                </thead>
-                @foreach($revaluo as $r)
-                <tbody>
-                    <tr>
-                        <td>{{ $r->CodBien }}</td>
-                        <td>{{ $r->NombreBien }}</td>
-                        <td>{{ $r->NombreCustodio }}</td>
-                        <td>{{ $r->NombreOperador }}</td>
-                        <td>{{ $r->Estado }}</td>
-                        <td>{{ $r->FechaHora }}</td>
-                        <td>{{ $r->Monto }}</td>
-                        <td>{{ $r->Descripcion }}</td>
-                    </tr>
-                </tbody>
-                @endforeach
-            </table>
-        </div> <!-- /.table-stats -->
-        {{$revaluo->render()}}
+</div>
+<!-- /# row -->
+<section id="main-content">
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="card">
+                <div class="bootstrap-data-table-panel">
+                    <div class="table-responsive">
+                        @include('RevisionTecnica.Mantenimiento.Search')
+                        <table id="bootstrap-data-table-export" class="table table-striped table-bordered">
+                            <thead>
+                                <tr>
+                                  <th>Cod. Bien</th>
+                                  <th>Nombre Bien</th>
+                                  <th>Custodio</th>
+                                  <th>Operador</th>
+                                  <th>Estado</th>
+                                  <th>FechaHora</th>
+                                  <th>Monto</th>
+                                  <th>Descripcion</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach($revaluo as $r)
+                                <tbody>
+                                    <tr>
+                                        <td>{{ $r->CodBien }}</td>
+                                        <td>{{ $r->NombreBien }}</td>
+                                        <td>{{ $r->NombreCustodio }}</td>
+                                        <td>{{ $r->NombreOperador }}</td>
+                                        <td>{{ $r->Estado }}</td>
+                                        <td>{{ $r->FechaHora }}</td>
+                                        <td>{{ $r->Monto }}</td>
+                                        <td>{{ $r->Descripcion }}</td>
+                                    </tr>
+                                </tbody>
+                                @endforeach
+                            </tbody>
+                        </table>
+                        {{$revaluo->render()}}
+                    </div>
+                </div>
+            </div>
+            <div>
+            <a href="/RevisionTecnica/reporteBaja"> <button class="btn btn-primary">descargar PDF</button> </a>
+            </div>
+            <!-- /# card -->
+        </div>
+        <!-- /# column -->
     </div>
-  </div>
+    <!-- /# row -->
+
+</section>
+
+
+
 
 @endsection
